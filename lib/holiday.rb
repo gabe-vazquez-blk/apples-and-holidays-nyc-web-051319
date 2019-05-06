358be13af3ed135d30f4337375ba807504674883
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-06 15:59:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,17 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  puts <<-TEXT
-Winter:
-  Christmas: Lights, Wreath
-  New Years: Party Hats
-Summer:
-  Fourth Of July: Fireworks, BBQ
-Fall:
-  Thanksgiving: Turkey
-Spring:
-  Memorial Day: BBQ
-  TEXT
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
